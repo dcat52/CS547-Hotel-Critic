a27199cbd7e2134b1664e97cf3991730d596465c
Davis <daviscatherman@gmail.com>
2021-05-09 11:48:19 -0400

    Add location search bar and address/price columns

--- project/models/SearchForm.py
+++ project/models/SearchForm.py
@@ -5,5 +5,6 @@ from wtforms.validators import Required
 
 class SearchForm(FlaskForm):
     qfield = TextField('Search Query')
+    lfield = TextField('Location Filter')
 
     submit_button = SubmitField('Search')
