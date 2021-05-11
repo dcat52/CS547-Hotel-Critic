6f5622ed098412d2499d605a9d709ea6039e7897
Davis <daviscatherman@gmail.com>
2021-05-11 11:59:15 -0400

    add eq fx to Hotel

--- project/models/Hotel.py
+++ project/models/Hotel.py
@@ -14,7 +14,14 @@ class Hotel(object):
         # self.key_words = {'service':'Service','clean':'Cleanliness','overall':'Overall','value':'Value','sleep quality':'Sleep Quality','room':'Rooms','location':'Location','internet':'Business service (e.g., internet access)','check in':'Check in / front desk'}
 
     def __str__(self):
-        return '{:40s}, Universal Score: {:.2f} '.format(self.name, self.us)
+        return '{:40s}, Universal Score: {:.2f}'.format(self.name, self.us, self.address)
 
     def __repr__(self):
-        return '\n{}'.format(str(self))
+        return '{}'.format(str(self))
+
+    def __eq__(self, other):
+        if self.name == other.name and self.address == other.address:
+            return True
+
+        return False
+
